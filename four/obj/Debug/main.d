c01354e72f3d42fc6f95e77e9d06dcfed0fa1dcc
Law Smith <sonokin@mit.edu>
2013-12-03 19:38:48 -0500

    Added code in triangle intersect to do parallax intersect.

--- four/obj/Debug/main.d
+++ four/obj/Debug/main.d
@@ -1,13 +1,12 @@
 obj/Debug/main.o: src/main.cpp src/SceneParser.h \
- vecmath/include/vecmath.h vecmath/include/Matrix2f.h \
- vecmath/include/Matrix3f.h vecmath/include/Matrix4f.h \
- vecmath/include/Quat4f.h vecmath/include/Vector2f.h \
- vecmath/include/Vector3f.h vecmath/include/Vector4f.h src/Camera.h \
- src/Ray.h vecmath/include/Vector3f.h src/Light.h src/Object3D.h \
- src/Hit.h src/Material.h src/texture.hpp src/bitmap_image.hpp \
- src/VecUtils.h src/Mesh.hpp src/Triangle.h vecmath/include/Vector2f.h \
- src/Group.h src/Sphere.h src/Plane.h src/Transform.h src/Image.h \
- src/POMUtils.h src/Segment.h
+  vecmath/include/vecmath.h vecmath/include/Matrix2f.h \
+  vecmath/include/Matrix3f.h vecmath/include/Matrix4f.h \
+  vecmath/include/Quat4f.h vecmath/include/Vector2f.h \
+  vecmath/include/Vector3f.h vecmath/include/Vector4f.h src/Camera.h \
+  src/Ray.h src/Light.h src/Object3D.h src/Hit.h src/Material.h \
+  src/texture.hpp src/bitmap_image.hpp src/VecUtils.h src/Mesh.hpp \
+  src/Triangle.h src/Segment.h src/POMUtils.h src/Group.h src/Sphere.h \
+  src/Plane.h src/Transform.h src/Image.h
 
 src/SceneParser.h:
 
@@ -31,8 +30,6 @@ src/Camera.h:
 
 src/Ray.h:
 
-vecmath/include/Vector3f.h:
-
 src/Light.h:
 
 src/Object3D.h:
@@ -51,7 +48,9 @@ src/Mesh.hpp:
 
 src/Triangle.h:
 
-vecmath/include/Vector2f.h:
+src/Segment.h:
+
+src/POMUtils.h:
 
 src/Group.h:
 
@@ -62,7 +61,3 @@ src/Plane.h:
 src/Transform.h:
 
 src/Image.h:
-
-src/POMUtils.h:
-
-src/Segment.h:
